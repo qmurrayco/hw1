14b3c106730e1ab0967c359efbb23f5303453b35
Quinton Murray-Conn <murrayco@usc.edu>
2023-01-28 06:45:31 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,96 @@
 
 int main(int argc, char* argv[])
 {
+	ULListStr dat;
+	dat.push_back("7");
+	dat.push_front("8");
+	dat.push_back("9");
 
+	std::cout << "------------------------TOP----------------------------------" << std::endl;
+
+	std::cout << "Get call index 0:" << dat.get(0) << std::endl; //should return 8
+	std::cout << "Get call index 1:" << dat.get(1) << std::endl; //should return 7
+	std::cout << "Get call index 2:" << dat.get(2) << std::endl; //should return 9
+	//std::cout << "Get call 5:" << dat.get(5) << std::endl; //should return NULL
+
+	dat.push_back("10");
+
+	std::cout << "Get call 3:" << dat.get(3) << std::endl; //should return 10
+
+	dat.push_front("11");
+
+	std::cout << "Get call index 0:" << dat.get(0) << std::endl; //should return 11
+
+	dat.pop_back(); //list should be 11 8 7  -> 9 -> NULL
+
+	std::cout << "Get call index 3:" << dat.get(3) << std::endl; //should return 9
+
+	dat.pop_front(); //list should be 8 7 -> 9 -> NULL
+
+	std::cout << "Get call index 0:" << dat.get(0) << std::endl; //should return 8
+
+	//lets see if we can delete the whole thing
+
+	std::cout << "-------------------------MID---------------------------------" << std::endl;
+
+	dat.pop_front(); //list should now be 7 -> 9 -> NULL
+
+	std::cout << "Get call index 0:" << dat.get(0) << std::endl; //should return 7
+	std::cout << "Get call index 1:" << dat.get(1) << std::endl; //should return 9
+
+	dat.pop_front(); //list should now be 9 -> NULL
+
+	std::cout << "Get call index 0:" << dat.get(0) << std::endl; //should return 9
+
+	dat.pop_front(); //list should be empty now
+
+	if(dat.empty()){
+		std::cout << "List is empty, deletion by pop_front was good" << std::endl;
+	}
+
+
+	//going to try the same with pop_back()
+	std::cout << "Attempting to refill list and delete again" << std::endl;
+	dat.push_back("7");
+	dat.push_front("8");
+	dat.push_back("9");
+
+	dat.pop_back();
+	dat.pop_back();
+	dat.pop_back();
+
+	if(dat.empty()){
+		std::cout << "List is empty, deletion by pop_back was good" << std::endl;
+	}
+	
+	dat.push_back("7");
+	dat.push_front("8");
+	dat.push_back("9");
+
+	std::cout << "List refilled, trying back() and front()" << std::endl;
+
+	std::string test;
+	test = dat.back();
+
+	std::cout << "Back() yields:" << test << std::endl; //should be 9
+
+	std::string test2;
+	test2 = dat.front();
+
+	std::cout << "Front() yields:" << test2 << std::endl; //should be 8
+
+	//50 elements from back
+
+	ULListStr list;
+
+	for(int i = 0; i < 50; i++){
+		list.push_back("test");
+	}
+
+
+	//try to delete them
+	
+	for(int i = 0; i < 50; i++){
+		list.pop_back();
+	}
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -1,6 +1,7 @@
 #include <cstddef>
 #include <stdexcept>
 #include "ulliststr.h"
+#include <iostream>
 
 ULListStr::ULListStr()
 {
@@ -62,4 +63,178 @@ void ULListStr::clear()
   }
   tail_ = NULL;
   size_ = 0;
+}
+
+void ULListStr::push_back(const std::string& val){
+	
+	//is dat list empty
+	if(empty()){
+		
+		head_ = new Item;
+		head_->val[9] = val;
+
+		head_->first = 9;
+		head_->last = 10;
+		size_++;
+
+		tail_ = head_;
+
+	}else{
+
+		//room at the end?
+		if(tail_->last == 10){
+			//nope
+			Item* ptr = new Item;
+
+			tail_->next = ptr;
+			ptr->prev = tail_;
+			tail_ = ptr;
+
+			tail_->first = 0;
+			tail_->val[tail_->first] = val;
+			tail_->last = 1;
+
+			size_++;
+
+			return;
+
+		}else{
+			//yep
+
+			tail_->val[tail_->last] = val;
+			size_++;
+			tail_->last++;
+
+			return;
+		}
+	}
+}
+
+void ULListStr::pop_back(){
+  
+  if(empty()){
+    //the list is empty, nothing to pop
+    return;
+  }else{
+    //the list still has data
+
+		//checking last node
+		if(tail_->last != 0){
+			//as long as its not empty just pop the last thing
+			tail_->last--;
+			size_--;
+
+			//did that make the node empty?
+			if(tail_->last == 0 && tail_->first == 0){
+				//if it is get rid of it
+				
+				if(head_ == tail_){
+					delete head_;
+					head_ = NULL;
+					tail_ = NULL;
+					return;
+
+				}else{
+					tail_ = tail_->prev;
+
+					delete tail_->next;
+
+					//bye felicia 
+					return;
+				}
+			}
+		}
+  }
+
+}
+
+void ULListStr::push_front(const std::string& val){
+	if(empty()){
+		head_ = new Item;
+		head_->val[9] = val;
+
+		head_->last = 10;
+		head_->first = 9;
+		size_++;
+
+		tail_ = head_;		
+	}else{
+		if(head_->first == 0){
+			Item* ptr = new Item;
+			ptr->val[9] = val;
+			ptr->last = 10;
+			ptr->first = 9;
+
+			ptr->next = head_;
+			head_->prev = ptr;
+			head_ = ptr;
+
+			size_++;
+		}else{
+			head_->first--;
+			head_->val[head_->first] = val;
+
+			size_++;
+		}
+	}
+}
+
+void ULListStr::pop_front(){
+	if(empty()){
+		return;
+	}else{
+		head_->first++;
+		size_--;
+
+		if(head_->first == head_->last){
+			if(head_->next == NULL){
+				delete head_;
+				head_ = NULL;
+				tail_ = NULL;
+			}else{
+				head_ = head_->next;
+				delete head_->prev;
+			}
+		}
+	}
+}
+
+std::string const & ULListStr::back() const{
+	return (tail_->val[tail_->last - 1]);
+}
+
+std::string const & ULListStr::front() const{
+	 return (head_->val[head_->first]);
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+	if(empty()){
+
+		return NULL;
+		
+	}else{
+
+		if((int)loc >= (int)size_){
+			return NULL;
+		}else{
+			int search = loc;
+			Item* walker = head_;
+
+			while(walker->next != NULL && (search - ((int)walker->last - (int)walker->first)) > 0){
+				search = search - ((int)walker->last - (int)walker->first);
+				walker = walker->next;
+			}	
+
+			if((int)walker->first + search > 9){
+				search = search - ((int)walker->last - (int)walker->first);
+				walker = walker->next;
+
+				return &walker->val[(int)walker->first + search];
+
+			}else{
+
+				return &walker->val[(int)walker->first + search];
+			}
+		}
+	}
 }
